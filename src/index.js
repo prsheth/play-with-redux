79f7c2851f4eceec7d69a0ff9016fdb3fbb02e51
Pranay <Pranay Sheth>
2016-11-26 18:43:59 -0500

    Init repo, setup boiler plate , created componenet structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+//YOUtube api key
+const API_KEY='AIzaSyCaKhEcLpPCRdVRiRRTWPqhHUR5VX_nv28';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//Create a new component and renders HTML
+const App=() =>{ //this is more of a class and connt be passed to the Dom hence need an instance of this class and then pass it to the DOM
+    //declared a constant in ES6
+    return <div>Hello</div>;  
+}//this is JSX dialect for javascript typically a react create element syntax
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+//Take components and render on page
+ReactDOM.render(<App />,document.querySelector('.container'));//an instance of APP and pass it to the DOM in the target container the root is the dom
